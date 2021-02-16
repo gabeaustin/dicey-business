f597b1cdee80be08370e1c5f6e55a6e8af83b117
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-02-15 21:44:17 -0600

    added a button to generate new die

---
+++ app.js
@@ -0,0 +1,10 @@
+class Die {
+    constructor() {
+        this.value = value;
+    }
+}
+
+// the "new" keyword
+// - creates a new empty object {}
+// - sets the value of "this" to be the new empty object
+// - calls the constructor method
