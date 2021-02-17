42f4b9d088d8336d7c9523d1d47e042ee4a69d0d
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-02-17 11:31:47 -0600

    got code working for when click Generate btn a random die shows on the screen

--- app.js
+++ app.js
@@ -1,43 +1,38 @@
-let min = 1;
-let max = 0;
+let minRollValue = 1;
+let maxRollValue = 6;
+let dieCount = 4;
+let diceArray = []
+let generateDieBtn = document.getElementById("generate-die-btn");
+let container = document.getElementById("dice-container");
 
 class Die {
-    constructor(x, y) {
+    constructor(value) {
+        this.value = value;
         this.div = document.createElement("div");
         this.div.classList.add("newDie");
-        this.div.style.left = `${x}px`;
-        this.div.style.top = `${y}px`;
+        this.roll(); // will change
+        this.div.innerText = this.value;
+        // below is how you call another method from inside a constructor
+        container.appendChild(this.div);
+        diceArray.push(this)
+        // this.div.style.left = `70px`; // ?
+        // this.div.style.top = `50px`; // ?
+    }
+    
+    roll() {
+        let randomValue = Math.floor(Math.random() * 6) + 1;
+        this.value = randomValue;
+        this.div.innerText = this.value;   
     }
 }
 
 // This code will generate a new div each time the button is clicked
-let generateDieBtn = document.getElementById("generate-die-btn");
 generateDieBtn.addEventListener("click", createDiv);
 
+
+// puts divs in a grid
 function createDiv() {
-
-};
-
-function roll(min, max) {
-    // Math.random() -> generates a # from 0 up to 1 (0.9999999)
-    // I want a number not larger than 6
-    return Math.floor(Math.random() * (max - min) + min);
+    new Die();
 };
 
 
-// class Die {
-//     constructor() {
-//         this.div = document.getElementById("dic-container");
-//         this.div.classList.add("button")
-//         this.value = value;
-//     }
-
-//     roll() {
-//         return Math.floor((Math.random() * 7));
-//     }
-// }
-
-
-// function randomVal() {
-//     return Math.floor((Math.random() * 7));
-// }
