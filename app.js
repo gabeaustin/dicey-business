be6c60a3c8fabf55138a58eaef3810e005ea0cba
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-02-17 13:14:06 -0600

    added dblclick event so when user double clicks a die that die will be removed

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-// let minRollValue = 1;
-// let maxRollValue = 6;
-// let dieCount = 4;
-let diceArray = []; // next to work on
-let generateDieBtn = document.getElementById("generate-die-btn");
 let container = document.getElementById("dice-container");
+let myVal = document.getElementById("dice-container");
+let generateDieBtn = document.getElementById("generate-die-btn");
+let rollDiceBtn = document.getElementById("roll-dice-btn");
+let diceArray = []; // WORK ON THIS NEXT
 
 class Die {
     constructor(value) {
@@ -14,23 +13,30 @@ class Die {
         this.div.innerText = this.value;
         // below is how you call another method from inside a constructor
         container.appendChild(this.div);
-        diceArray.push(this)
-        // this.div.style.left = `70px`; // ?
-        // this.div.style.top = `50px`; // ?
+        diceArray.push(this); // WORK ON THIS NEXT
+        // this.div.classList.add("rollDice";)
+        this.div.addEventListener("dblclick", function() {
+            this.div.remove();
+        }.bind(this));
     }
     
     roll() {
         let randomValue = Math.floor(Math.random() * 6) + 1;
         this.value = randomValue;
-        this.div.innerText = this.value;   
+        this.div.innerText = this.value;
     }
 }
 
 // This code will generate a new div each time the button is clicked
 generateDieBtn.addEventListener("click", createDiv);
+rollDiceBtn.addEventListener("dblclick", updateDiv);
 
-// puts divs in a grid
 function createDiv() {
+    new Die();
+    // console.log(this.value); 
+};
+
+function updateDiv() {
     new Die();
 };
 
